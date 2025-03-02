48739a839ebf6425d9cc712d36ba558488737256
SaymonDouglasDev <saymondouglasdev@gmail.com>
2025-03-01 22:58:22 -0300

    feat: added in a button to access the restaurant in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <h1>Hello World</h1>;
+  const router = useRouter();
+  const handleRedirectClick = () => router.push(`/fsw-donalds`)
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* LOGO E TITULO */}
+      <div className="flex flex-col items-center gap-2">
+        <h2 className="text-4xl font-bold">fsw-donalds</h2>
+      </div>
+      <div className="space-y-2 pt-24 text-center">
+        <Button 
+          variant="destructive" 
+          className="w-full rounded-full"
+          onClick={handleRedirectClick}
+        >
+          Clique aqui para entrar
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
